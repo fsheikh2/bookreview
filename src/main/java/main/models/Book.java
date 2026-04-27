dc782965562952dce45f5e49e002a930714882c0
Fahim Sheikh <fsheikh2@illinois.edu>
2026-04-26 20:59:01 -0500

    Added a getAverageRating method

--- src/main/java/main/models/Book.java
+++ src/main/java/main/models/Book.java
@@ -25,4 +25,6 @@ public class Book {
 
     public String getAuthor() { return author; }
     public void setAuthor(String author) { this.author = author; }
+
+    public List<Review> getReviews() { return reviews; }
 }
--- src/main/java/main/services/BookService.java
+++ src/main/java/main/services/BookService.java
@@ -1,6 +1,7 @@
 package main.services;
 
 import main.models.Book;
+import main.models.Review;
 import main.repositories.BookRepository;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +19,15 @@ public class BookService {
         book.setTitle(title);
         book.setAuthor(author);
         return bookRepository.save(book);
+    }
+
+    public double getAverageRating(Long bookId){
+        Book book = bookRepository.findById(bookId).orElseThrow( () -> new RuntimeException("Book not found"));
+
+        return book.getReviews()
+                .stream()
+                .mapToInt(Review::getRating)
+                .average()
+                .orElse(0.0);
     }
 }
